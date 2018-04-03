6db83ca66bfd1f44660da5930c83922dc43308c2
Connie <cmzhang96@gmail.com>
2018-04-03 17:04:39 -0400

    set freq and duty cycles

--- RobotListener.py
+++ RobotListener.py
@@ -12,7 +12,7 @@ import sys
 import SocketWrapper
 import pigpio
 
-#pi = pigpio.pi()
+pi = pigpio.pi()
 
 
 def deparseKiwi( msg ):
@@ -25,14 +25,22 @@ def deparseKiwi( msg ):
 	pwm1 = inputs[0]
 	pwm2 = inputs[1]
 	pwm3 = inputs[2]
+	setPWM(pwm1, pwm2, pwm3)
 
 def setPWM(pwm1, pwm2, pwm3):
-	pi.set_PWM_dutycycle(18, pwm1)
-	pi.set_PWM_dutycycle(12, pwm2)
-	pi.set_PWM_dutycycle(14, pwm3)
+	pi.set_PWM_dutycycle(13, pwm1)
+	pi.set_PWM_dutycycle(18, pwm2)
+	pi.set_PWM_dutycycle(19, pwm3)
+	str1 = "set: " + str(pwm1) + ", " + str(pwm2) + ", " + str(pwm3)
+
+def initPWMfreq():
+	pi.set_PWM_frequency(13, 50000)
+	pi.set_PWM_frequency(18, 50000)
+	pi.set_PWM_frequency(19, 50000)
 
 if __name__ == '__main__':
 	s = SocketWrapper.SocketWrapper(is_listener=True, socket_info=('localhost', 5020))
+	initPWMfreq()
 	try:
 	    while True:
 	        message = s.get_message()
