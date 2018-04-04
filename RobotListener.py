bbab4ce654273fc71e51bd86aa557df1b6bb7e17
Connie <cmzhang96@gmail.com>
2018-04-04 19:59:26 -0400

    PWM

--- RobotListener.py
+++ RobotListener.py
@@ -14,42 +14,74 @@ import pigpio
 
 #pi = pigpio.pi()
 
+def setKiwiOutput( msg ):
+	pwm1, pwm2, pwm3 = [float(t) for t in msg[1:-1].split(",")]
+	
+	#set orientation pins
+	if pwm1 > 0:
+		pi.write(5, 1)
+		pi.write(6, 0)
+	else if pwm1 < 0
+		pi.write(5, 0)
+		pi.write(6, 1)
+	else
+		pi.write(5, 0)
+		pi.write(6, 0)
 
-def deparseKiwi( msg ):
-	inputs = []
-	for t in s.split():
-	    try:
-	        inputs.append(float(t))
-	    except ValueError:
-	        pass
-	pwm1 = inputs[0]
-	pwm2 = inputs[1]
-	pwm3 = inputs[2]
-	setPWM(pwm1, pwm2, pwm3)
+	if pwm2 > 0
+		pi.write(23, 1)
+		pi.write(24, 0)
+	else if pwm2 < 0
+		pi.write(23, 0)
+		pi.write(24, 1)
+	else
+		pi.write(23, 0)
+		pi.write(24, 0)
 
-def setPWM(pwm1, pwm2, pwm3):
-	pi.set_PWM_dutycycle(13, pwm1)
-	pi.set_PWM_dutycycle(18, pwm2)
-	pi.set_PWM_dutycycle(19, pwm3)
+	if pwm3 > 0
+		pi.write(26, 1)
+		pi.write(20, 0)
+	else if pwm3 < 0
+		pi.write(26, 0)
+		pi.write(20, 1)
+	else
+		pi.write(26, 0)
+		pi.write(20, 0)
+
+	#set the duty cycle
+	pi.set_PWM_dutycycle(13, abs(pwm1))
+	pi.set_PWM_dutycycle(18, abs(pwm2))
+	pi.set_PWM_dutycycle(19, abs(pwm3))
+
+	print("duty cycles set")
 	str1 = "set: " + str(pwm1) + ", " + str(pwm2) + ", " + str(pwm3)
+	print(str1)
 
-def initPWMfreq():
+def initPWM():
 	pi.set_PWM_frequency(13, 50000)
 	pi.set_PWM_frequency(18, 50000)
 	pi.set_PWM_frequency(19, 50000)
+	#configure the output pins for motor direction
+	pi.set_mode(5, pigpio.OUTPUT) # clockwise motor 1
+	pi.set_mode(6, pigpio.OUTPUT) # cc motor 1
+	pi.set_mode(23, pigpio.OUTPUT) # clockwise motor 2
+	pi.set_mode(24, pigpio.OUTPUT) # cc motor 2
+	pi.set_mode(26, pigpio.OUTPUT) # clockwise motor 3
+	pi.set_mode(20, pigpio.OUTPUT) # cc motor 3
 
 if __name__ == '__main__':
 	s = SocketWrapper.SocketWrapper(is_listener=True, socket_info=('', 5020))
-	initPWMfreq()
+	initPWM()
 	try:
 	    while True:
 	        message = s.get_message()
 	        s.send_data(message[-1])
-	        if message[0] == '[':
+	        if message[-1][0] == '[':
 	        	print("got vel, deparsing kiwi")
-	        	deparseKiwi(message)
+	        	setKiwiOutput(message[-1])
 	        else:
-	        	print("message only" + message)
+	        	print("message only: ")
+	        	print(message)
 	except KeyboardInterrupt:
 	    s.close_socket()
 	    sys.exit()
